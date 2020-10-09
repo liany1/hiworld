f70a6879d55ec6a54d7c0e74020464e811cad628
https://github.com/liany1/hiworld <1105135592@qq.com>
2020-10-09 14:35:17 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,6 @@
 public class HiWorld
 {
     public static void main(String args[]){
-        System.out.println("Hioii8idasdii World");
+        System.out.println("Hioiiasdasd8idasdii World");
     }
 }
